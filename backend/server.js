1797babc080ea9ceaf354ef94d80d012baa19315
Tukur-eng <tukurakorede@gmail.com>
2023-06-21 04:03:19 +0800

    Add productDetails to redux

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,22 @@ import data from './data.js';
 
 
 
+
+
+
+
+
 const app = express();
+
+app.get('/api/products/:id', (req, res) => {
+    const product = data.products.find( (x) => x._id === req.params.id);
+    if(product){
+    res.send(product);
+}else {
+    res.status(404).send({message: 'Product Not Found'});
+}
+});
+
 app.get('/api/products', (req, res) => {
     res.send(data.products);
 });
@@ -13,5 +28,7 @@ app.get('/', (req, res) => {
 });
 
 const port = process.env.PORT || 5000;
-app.listen(port, ()=>{console.log(`serve at http://localhost:${port}`);
+
+app.listen(port, ()=>{
+    console.log(`serve at http://localhost:${port}`);
 });
