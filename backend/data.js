74039292518acf253864b5b3561df0d6481ebcdf
Tukur-eng <tukurakorede@gmail.com>
2023-06-24 06:18:53 +0800

    SigninScreen,CheckoutScreen and PaymentmethodScreen

--- backend/data.js
+++ backend/data.js
@@ -1,4 +1,19 @@
+import bcrypt from 'bcryptjs';
 const data = {
+  users: [
+    {
+      name: 'Tukur',
+      email: 'admin@example.com',
+      password: bcrypt.hashSync('1234', 8),
+      isAdmin: true,
+    },
+    {
+      name: 'Fatiu',
+      email: 'user@example.com',
+      password: bcrypt.hashSync('1234', 8),
+      isAdmin: false,
+    }
+],
   products: [
     {
       _id: '1',
--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,34 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import data from './data.js';
-
-
-
-
-
-
-
+import userRouter from './routers/userRouter.js';
 
 const app = express();
+mongoose.connect('mongodb://127.0.0.1:27017/vitabay');
 
 app.get('/api/products/:id', (req, res) => {
-    const product = data.products.find( (x) => x._id === req.params.id);
-    if(product){
+  const product = data.products.find((x) => x._id === req.params.id);
+  if (product) {
     res.send(product);
-}else {
-    res.status(404).send({message: 'Product Not Found'});
-}
+  } else {
+    res.status(404).send({ message: 'Product Not Found' });
+  }
 });
 
 app.get('/api/products', (req, res) => {
-    res.send(data.products);
+  res.send(data.products);
 });
+app.use('/api/users', userRouter);
 
 app.get('/', (req, res) => {
-    res.send('server is ready');
+  res.send('server is ready');
 });
 
-const port = process.env.PORT || 5000;
+app.use((err, req, res) => {
+    res.status(500).send({ message: err.message });
+  });
 
-app.listen(port, ()=>{
-    console.log(`serve at http://localhost:${port}`);
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`serve at http://localhost:${port}`);
 });
