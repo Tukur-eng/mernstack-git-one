b8e37113f47f27cf85b90ae1e04d8c9a15ac9320
Tukur-eng <tukurakorede@gmail.com>
2023-06-24 15:51:31 +0800

    PaymentMethod and PlaceOrder

--- backend/server.js
+++ backend/server.js
@@ -1,34 +1,32 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import data from './data.js';
+import dotenv from 'dotenv';
 import userRouter from './routers/userRouter.js';
+import productRouter from './routers/productRouter.js';
 
+
+
+dotenv.config();
 const app = express();
-mongoose.connect('mongodb://127.0.0.1:27017/vitabay');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/api/products/:id', (req, res) => {
-  const product = data.products.find((x) => x._id === req.params.id);
-  if (product) {
-    res.send(product);
-  } else {
-    res.status(404).send({ message: 'Product Not Found' });
-  }
-});
+mongoose.connect(process.env.MONGODB_URL ||'mongodb://127.0.0.1:27017/vitabay',{
+    
 
-app.get('/api/products', (req, res) => {
-  res.send(data.products);
+useNewUrlParser: true,
+useUnifiedTopology: true,
+
 });
 app.use('/api/users', userRouter);
-
+app.use('/api/products', productRouter);
 app.get('/', (req, res) => {
-  res.send('server is ready');
+res.send('Server is ready');
 });
-
-app.use((err, req, res) => {
-    res.status(500).send({ message: err.message });
-  });
-
+app.use((err, req, res, next) => {
+res.status(500).send({ message: err.message });
+});
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`serve at http://localhost:${port}`);
+console.log(`Serve at http://localhost:${port}`);
 });
